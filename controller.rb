4a8a377744b63dd630a56fdb11985549180c12cd
Alden Sulger <asulger@lsoc.org>
2016-04-22 10:29:07 -0500

    first try getting it

--- controller.rb
+++ controller.rb
@@ -8,11 +8,18 @@ get '/' do
 end
 
 post '/part_two' do
-	if "Dropout" == 
-		@title = "You may want to rethink!"
+	if "Dropout" == params[:@choice]
+	@title = "You may want to rethink!"
+		
+		@choice_1 = "Choose whats next"
+		@choice_2 = "Get G.E.D."
+		@choice_3 = "Live With Your parents"
 		erb :part_two
-	elsif "Graduate"
-		@title = "Good Choice!"
+	elsif "Graduate" == params[:@choice]
+	then	@title = "Good Choice! Now what's next in your academic endeavors?"
+		@choice_1 = "Community College"
+		@choice_2 = "4-year university"
+		@choice_3 = "_________ School"
 		erb :part_two
 	end
 end
